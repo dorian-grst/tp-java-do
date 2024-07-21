5a940ad4446dc7d9044f6ead7b774bb2f19e4b5f
Dorian Grasset <dorian.grasset.contact@gmail.com>
2024-07-21 19:10:03 +0200

    remove useless class & code

--- src/main/java/app/Team.java
+++ src/main/java/app/Team.java
@@ -10,15 +10,7 @@ public class Team {
         return teamName;
     }
 
-    public void setTeamName(TeamName teamName) {
-        this.teamName = teamName;
-    }
-
     public List<ChampionDistribution> getChampionsDistribution() {
         return championsDistribution;
-    }
-
-    public void setChampionsDistribution(List<ChampionDistribution> championsDistribution) {
-        this.championsDistribution = championsDistribution;
     }
 }
